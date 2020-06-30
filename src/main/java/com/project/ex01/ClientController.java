130bdc9e95e37ebefdb399032384a72dff643f03
zeratool0207 <zeratool0207@naver.com>
2020-06-30 16:04:55 +0900

    no message

--- src/main/java/com/project/ex01/ClientController.java
+++ src/main/java/com/project/ex01/ClientController.java
@@ -5,11 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-<<<<<<< HEAD
-// push branch
-=======
 // master push
->>>>>>> master
 
 
 @Controller
