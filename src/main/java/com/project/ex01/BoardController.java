8904d7230d69cdb2efb36848b64efaa0a5c48795
BrooklynKim <zflippurple@gmail.com>
2020-08-04 10:40:50 +0900

    200804 not yet push

--- src/main/java/com/project/ex01/BoardController.java
+++ src/main/java/com/project/ex01/BoardController.java
@@ -1,19 +1,15 @@
 package com.project.ex01;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -200,6 +196,8 @@ public class BoardController {
 		SimpleDateFormat fm = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		
 		bv.setRegdate(fm.format(current));
+		
+		MulitipartFile uploadfile;
 		
 		if(id != null) {
 			bv.setId(id);
