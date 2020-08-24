161030aa51c19ed90998f1593ad0a18b8e7f49ff
Sherry90 <hhlsdkm@gmail.com>
2020-08-24 14:55:55 +0900

    0824 image commit

--- src/main/java/vo/ProductImageVO.java
+++ src/main/java/vo/ProductImageVO.java
@@ -3,11 +3,9 @@ package vo;
 public class ProductImageVO {
 	private String productcode;
 	private String filename;
+	private boolean ismain;
 	public String getProductcode() {
 		return productcode;
-	}
-	public void setProductcode(String productcode) {
-		this.productcode = productcode;
 	}
 	public String getFilename() {
 		return filename;
@@ -15,8 +13,19 @@ public class ProductImageVO {
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
+	public boolean isIsmain() {
+		return ismain;
+	}
+	public void setIsmain(boolean ismain) {
+		this.ismain = ismain;
+	}
+	public void setProductcode(String productcode) {
+		this.productcode = productcode;
+	}
 	@Override
 	public String toString() {
-		return "ProductImageVO [productcode=" + productcode + ", filename=" + filename + "]";
+		return "ProductImageVO [productcode=" + productcode + ", filename=" + filename + ", ismain=" + ismain + "]";
 	}
+	
+	
 }
