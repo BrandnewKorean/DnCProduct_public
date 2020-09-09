2fa40013c88cbb8072d4df4e3851766b25cd02ce
PC <PC@DESKTOP-GLEMP0L>
2020-09-09 15:38:54 +0900

    Commit

--- src/main/webapp/resources/script/storemain.js
+++ src/main/webapp/resources/script/storemain.js
@@ -1,6 +1,6 @@
+var banner = 0;
+var interval = 0;
 $(function(){
-	var banner = 0;
-		
 	$('.slide_item').css({
 		width: (window.innerWidth * 0.9 -1)+'px',
 		height: 400+'px'
@@ -25,23 +25,8 @@ $(function(){
 	
 	$('#banner_left').css('visibility','hidden');
 	
-	setInterval(function(){
-		banner++;
-		if(banner > 3) banner = 0;
-		if(banner <= 0){
-			$('#banner_left').css('visibility','hidden');
-			$('#banner_right').css('visibility','visible');
-		}else if(banner < 3){
-			$('#banner_left').css('visibility','visible');
-			$('#banner_right').css('visibility','visible');
-		}else{
-			$('#banner_left').css('visibility','visible');
-			$('#banner_right').css('visibility','hidden');
-		}
-		$('.slide_wrap').css({
-			transform: "translate(-"+(window.innerWidth * 0.9)*banner+"px)"
-		});
-	}, 5000);
+	interval = setInterval('start()', 3000);
+	console.log('interval = ',interval);
 	
 	$('#banner_left').click(function(){
 		banner--;
@@ -55,6 +40,9 @@ $(function(){
 		$('.slide_wrap').css({
 			transform: "translate(-"+(window.innerWidth * 0.9)*banner+"px)"
 		});
+		setTimeout(function(){
+			interval = setInterval('start()',3000);
+		},1000);
 	});
 	
 	$('#banner_right').click(function(){
@@ -69,5 +57,31 @@ $(function(){
 		$('.slide_wrap').css({
 			transform: "translate(-"+(window.innerWidth * 0.9)*banner+"px)"
 		});
+		setTimeout(function(){
+			interval = setInterval('start()',3000);
+		},1000);
 	});
 });
+
+function start(){
+	banner++;
+	if(banner > 3) banner = 0;
+	if(banner <= 0){
+		$('#banner_left').css('visibility','hidden');
+		$('#banner_right').css('visibility','visible');
+	}else if(banner < 3){
+		$('#banner_left').css('visibility','visible');
+		$('#banner_right').css('visibility','visible');
+	}else{
+		$('#banner_left').css('visibility','visible');
+		$('#banner_right').css('visibility','hidden');
+	}
+	$('.slide_wrap').css({
+		transform: "translate(-"+(window.innerWidth * 0.9)*banner+"px)"
+	});
+}
+
+function stop(){
+	console.log('stop interval = '+interval);
+	clearInterval(interval);
+}
